7706e92b9ba9bb1caa3a126c3f316a2628e9fccd
Maria  Morris <mariasemailbox@gmail.com>
2018-05-04 03:53:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,12 @@
 # Your code goes here!
 class Dog
+def name=(name_of_dog)
+  @name = name_of_dog
+end
+def name
+  @name
+end
   def bark
     puts "Woof!"
   end
-  def name=(name)
-   @name = name
  end
- def name
-   @name
- end
- end
-
- fido.name
- fido.bark
